db7381f42a453d8563227323ee0c0e412bf5bce7
galit <galit1321@gmail.com>
2017-12-06 10:04:52 +0200

    commit all

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express')
 var passport = require('passport');
 var session = require('express-session');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -25,8 +24,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));//WHERE STATIC RESOURCES R KEPT
 
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
 app.use('/', index);
 
 
