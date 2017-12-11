0eccff8870a688c48dd2beab0989be71bd8f7af5
galit <galit1321@gmail.com>
2017-12-11 13:42:31 +0200

    commit all

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-var valider= require('express-validator')
+//var valider= require('express-validator')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Registration' });
 
 });
 router.get('/login', function(req, res, next) {
-    res.render('index', { title: 'User' });
+    res.render('bank', { title: 'User' });
 
 });
-router.get('/register', function(req, res, next) {
-    res.render('index', { title: 'User' });
 
-});
 
 var users=[]
 router.post('/register',function (req,res,next) {
@@ -26,13 +23,15 @@ router.post('/register',function (req,res,next) {
     users.push(user);
     res.render('login',{title: 'title'})
 })
-router.post('/login',passport.authenticate('local',{
-    successRedirect: '/bank',
-    failureRedirect: '/login' }));
+router.post('/login',
+    passport.authenticate('local', { failureRedirect: '/login' }),
+    function(req, res) {
+        res.redirect('/bank');
+    });
 
 
 router.get('/bank',authenticationMiddleware(), function (req,res,next) {
-    res.render('error',{title:'ffff'})
+    res.render('bank',{title:'ffff'})
 })
 function authenticationMiddleware () {
     return function (req, res, next) {
@@ -43,13 +42,15 @@ function authenticationMiddleware () {
     }
 }
 
-passport.serializeUser(function(username, done) {
-    done(null, username);
+passport.serializeUser(function(user, cb) {
+    cb(null, user.id);
 });
 
-passport.deserializeUser(function(username, done) {
-        done(null, username);
-
+passport.deserializeUser(function(id, cb) {
+    db.users.findById(id, function (err, user) {
+        if (err) { return cb(err); }
+        cb(null, user);
+    });
 });
 module.exports = router;
 
